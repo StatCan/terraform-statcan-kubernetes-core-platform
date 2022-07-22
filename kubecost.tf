50637532af27bb1879bd19a3de2dd5a7e0e80e90
Will Hearn <will.hearn@cloud.statcan.ca>
2022-07-22 01:42:20 +0000

    feat: resolves CN-1048 and CN-1054

--- kubecost.tf
+++ kubecost.tf
@@ -34,14 +34,14 @@ module "namespace_kubecost_system" {
   # Dependencies
   dependencies = []
 }
-
 module "kubecost" {
   providers = {
     helm = helm
   }
 
-  source = "git::https://github.com/statcan/terraform-kubernetes-kubecost.git?ref=v3.x"
+  source = "git::https://github.com/statcan/terraform-kubernetes-kubecost.git?ref=v3.1.0"
 
+  chart_version = "1.92.0"
   depends_on = [
     kubernetes_namespace.kubecost_system
   ]
@@ -52,103 +52,47 @@ module "kubecost" {
   helm_repository_password = var.platform_helm_repository_password
 
   values = <<EOF
-# Default values for cost-analyzer.
-# This is a YAML-formatted file.
-# Declare variables to be passed into your templates.
-
 global:
-  savedReports:
+  prometheus:
     enabled: false
-
-  assetReports:
+    fqdn: http://kube-prometheus-stack-prometheus.prometheus-system.svc:9090
+  grafana:
     enabled: false
-
-  notifications:
-    alertConfigs:
-      frontendUrl: "kubecost.${var.ingress_domain}"
-      globalSlackWebhookUrl: "${var.kubecost_slack_token}"
-      globalAlertEmails:
-        - brendan.gadd@canada.ca
-        - william.hearn@canada.ca
-        - zachary.seguin@canada.ca
-
-      alerts:
-        - type: budget
-          threshold: 1000
-          window: daily
-          aggregation: cluster
-          filter: cluster-one
-
-        - type: budget
-          threshold: 50
-          window: daily
-          aggregation: namespace
-          filter: "minio-operator-system,minio-premium-system,minio-standard-system"
-          ownerContact:
-            - william.hearn@canada.ca
-            - zachary.seguin@canada.ca
-
-        - type: recurringUpdate
-          window: weekly
-          aggregation: namespace
-          filter: '*'
-
-        - type: spendChange
-          relativeThreshold: 0.20
-          window: 1d
-          baselineWindow: 30d
-          aggregation: namespace
-          filter: "${var.kubecost_shared_namespaces}"
-
-        - type: health
-          window: 10m
-          threshold: 5
-
-        - type: diagnostic
-          window: 10m
-
-    alertmanager:
-      enabled: false
-
+    domainName: kube-prometheus-stack-grafana.prometheus-system
+    proxy: false
 kubecostToken: "${var.kubecost_token}"
-
 imagePullSecrets:
   - name: "${local.platform_image_pull_secret_name}"
-
 kubecostFrontend:
   image: "${local.repositories.gcr}kubecost1/frontend"
-
 kubecost:
   image: "${local.repositories.gcr}kubecost1/server"
-
 kubecostModel:
   image: "${local.repositories.gcr}kubecost1/cost-model"
-
 remoteWrite:
   postgres:
     initImage: "${local.repositories.gcr}kubecost1/sql-init"
-
 networkCosts:
-  image: "${local.repositories.gcr}kubecost1/kubecost-network-costs:v15.6"
-
+  image: "${local.repositories.gcr}kubecost1/kubecost-network-costs:v16.0"
 clusterController:
-  image: "${local.repositories.gcr}kubecost1/cluster-controller:v0.0.2"
-
+  image: "${local.repositories.gcr}kubecost1/cluster-controller:v0.1.0"
 initChownDataImage: "${local.repositories.dockerhub}busybox"
-
+ingress:
+  enabled: true
+  hosts:
+  - "kubecost.${var.ingress_domain}"
+  paths:
+  - /
+  pathType: Prefix
 tolerations:
   - key: CriticalAddonsOnly
     operator: Exists
-
-ingress:
-  enabled: true
-  annotations:
-    kubernetes.io/ingress.class: istio
-  hosts:
-    - "kubecost.${var.ingress_domain}"
-  paths:
-    - '/*'
-
+grafana:
+  sidecar:
+    dashboards:
+      enabled: true
+    datasources:
+      enabled: false
 prometheus:
   nodeExporter:
     tolerations:
@@ -156,54 +100,34 @@ prometheus:
         operator: Exists
       - effect: NoSchedule
         operator: Exists
-
-  alertmanager:
-    enabled: true
-
 %{if length(var.kubecost_prometheus_node_selector) > 0~}
   nodeSelector:
     ${indent(4, yamlencode(var.kubecost_prometheus_node_selector))~}
 %{endif~}
-
 kubecostProductConfigs:
   clusterName: "${var.cluster_name}"
   clusterProfile: ${var.kubecost_cluster_profile}
   currencyCode: "CAD"
-  labelMappingConfigs:
-    enabled: true
-    # owner_label: "owner"
-    # team_label: "team"
-    # department_label: "dept"
-    product_label: "finance.statcan.gc.ca/workload-id"
-    # environment_label: "env"
-    # namespace_external_label: "kubernetes_namespace"
-    # cluster_external_label: "kubernetes_cluster"
-    # controller_external_label: "kubernetes_controller"
-    product_external_label: "wid"
-    # service_external_label: "kubernetes_service"
-    # deployment_external_label: "kubernetes_deployment"
-    # owner_external_label: "kubernetes_label_owner"
-    # team_external_label: "kubernetes_label_team"
-    # environment_external_label: "kubernetes_label_env"
-    # department_external_label: "kubernetes_label_department"
-    # statefulset_external_label: "kubernetes_statefulset"
-    # daemonset_external_label: "kubernetes_daemonset"
-    # pod_external_label: "kubernetes_pod"
-  gpuLabel: "node.statcan.gc.ca/use"
-  gpuLabelValue: "gpu"
   azureBillingRegion: CA
   azureSubscriptionID: ${var.subscription_id}
   azureClientID: ${var.kubecost_client_id}
   azureClientPassword: ${var.kubecost_client_secret}
   azureTenantID: ${var.tenant_id}
-  azureStorageAccount: ${var.kubecost_storage_account}
-  azureStorageAccessKey: ${var.kubecost_storage_access_key}
-  azureStorageContainer: ${var.kubecost_storage_container}
-  azureStorageCreateSecret: true
-  sharedNamespaces: "${var.kubecost_shared_namespaces}"
+  createServiceKeySecret: true
+  grafanaURL: https://grafana.${var.ingress_domain}
   productKey:
     enabled: true
     key: ${var.kubecost_product_key}
-  createServiceKeySecret: true
+  labelMappingConfigs:
+    enabled: true
+    owner_label: "project.statcan.gc.ca/lead"
+    team_label: "project.statcan.gc.ca/team"
+    department_label: "project.statcan.gc.ca/division"
+    product_label: "finance.statcan.gc.ca/workload-id"
+    product_external_label: "wid"
+    environment_label: "project.statcan.gc.ca/environment"
+  gpuLabel: "node.statcan.gc.ca/use"
+  gpuLabelValue: "gpu"
+  sharedNamespaces: "${var.kubecost_shared_namespaces}"
 EOF
 }
